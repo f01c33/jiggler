6282df85366a4ef937e1e0ecebd244156aecbadd
f01c33 <felchar@proton.me>
2024-05-25 17:43:23 -0300

    new flag, toggle

--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"flag"
 	"time"
 
 	"github.com/go-vgo/robotgo"
 )
 
 func main() {
+	toggle := flag.Bool("t", false, "toggle, used for keeping the jiggler on, if set to true putting the mouse in the top-left coorner will toggle the jiggling")
+	isJiggling := true
+	flag.Parse()
 	robotgo.MouseSleep = 100
 	sx, _ := robotgo.GetScreenSize()
 	for {
-		robotgo.Move(sx, 10)
-		robotgo.Move(sx, 11)
+		if isJiggling {
+			robotgo.Move(sx, 10)
+			robotgo.Move(sx, 11)
+		}
 		time.Sleep(time.Second)
 		locX, locY := robotgo.Location()
 		if locX >= sx-8 && locY <= 8 {
-			break
+			if !*toggle {
+				break
+			} else {
+				isJiggling = !isJiggling
+			}
 		}
 	}
 }
